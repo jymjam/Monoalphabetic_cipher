b097ed0608bb5d44daf0b447f86dfc2d0a66c627
white-panda01 <hars0101@gmail.com>
2021-10-09 02:00:47 +0400

    created guessed key

--- cipher.py
+++ cipher.py
@@ -2,30 +2,28 @@
 Author: HK
 About: Implementation of monoalphabetic cipher
 '''
+from collections import Counter
 import random, string
 import sys
-from collections import Counter
 
 #func creates a random key value pair for encryption
-def buildCipher(key = None):
-    keyPool = string.ascii_lowercase + string.digits + string.punctuation#only numbers and alpha; need to add symbols
+def buildCipher():
+    keyPool = string.ascii_lowercase + string.digits + string.punctuation + string.whitespace #only numbers and alpha;
     alpha = list(keyPool) # list of alphabets ['a','b','c','d',....'8','9']
     cipher = list(keyPool) # creates another list same as alpha above
     random.shuffle(cipher) # shuffles cipher list ['z','c','5','l',....,'6','k']
-
     encCipher = dict(zip(alpha, cipher)) #creates a {alpha:cipher} dictionary {'a':'z','b':'c',...,'8':'6','9':'k'}
-
-    return encCipher #returns the pair (encryption key)
+    return encCipher 
 
 encryption_key_pair = buildCipher() # returned dict of buildCipher is the encryption key 
 decryption_key_pair = dict(map(reversed, encryption_key_pair.copy().items())) #copies encryption key and reverses it
-print(encryption_key_pair)
+
 #function to encrypt ascii passed in the argument 
 def encrypt(text_to_encrypt, eKey): #takes a text and the encryption key
     encrypted_text = []
     for i in text_to_encrypt:
         encrypted_text.append(eKey.get(i,i))
-    return ''.join(encrypted_text) #encypted text is returned
+    return ''.join(encrypted_text)
 
 #function to decrypt ascii passed in the argumetn; for testing
 def decrypt(cipher, dKey): #takes a text and decryption key
@@ -34,30 +32,37 @@ def decrypt(cipher, dKey): #takes a text and decryption key
         decrypted_text.append(dKey.get(i,i))
     return ''.join(decrypted_text)
 
-#function to count the frequency of the program
+def realTextCounter(plainText):
+    counted = Counter(plainText)
+    return counted
 
-def cryptanalysis(cipher):
-    pool = list(string.ascii_lowercase + string.digits + string.punctuation)
+#function to count the frequency of the program
+def cryptAnalysis(cipher):
+    pool = list(string.ascii_lowercase + string.digits + string.punctuation + string.whitespace)
     counted = Counter(cipher)
     real_count_dict = {k: counted[k] for k in pool if k in counted}
-    print(counted)
-    print(real_count_dict)
+    return real_count_dict
 
 #main func 
 def main():
     #check if file in argument
     if len(sys.argv) < 2: #checks if the argumetn has a passed file to encrypt
-        print("example: python3 ./cipher ./encrypt_this.java")
+        print("example: python3 ./cipher ./file_to_encrypt")
         quit()
 
     oFile = open(sys.argv[1], 'r') #if passed; the file is opened to read
     fileContent = oFile.read()
-
-    tmp = encrypt(fileContent, encryption_key_pair) #content of the opend file is encrypted
-    #print(tmp)
+    encryptedText = encrypt(fileContent, encryption_key_pair) #content of the opend file is encrypted
     oFile.close()
 
-    print(cryptanalysis(tmp))
+    realTextCount = realTextCounter(fileContent)
+    cryptCount = cryptAnalysis(encryptedText)
+
+    guessedKey = dict(zip(cryptCount, realTextCount))
+
+    print(decryption_key_pair)
+    print("--------------------------------------------------------------------------------")
+    print(decrypt(encryptedText, guessedKey))
 
 #if not imported
 if __name__ == '__main__':
