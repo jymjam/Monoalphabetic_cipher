da60f83fe0b7c995977972391c7a2bfe463c9613
white-panda01 <hars0101@gmail.com>
2021-10-09 02:33:37 +0400

    added menu

--- cipher.py
+++ cipher.py
@@ -31,17 +31,18 @@ def decrypt(cipher, dKey): #takes a text and decryption key
     for i in cipher:
         decrypted_text.append(dKey.get(i,i))
     return ''.join(decrypted_text)
+#-----------------------------------------------------DO NOT TOUCH ABOVE--------------------------------
 
-def realTextCounter(plainText):
-    counted = Counter(plainText)
-    return counted
-
-#function to count the frequency of the program
-def cryptAnalysis(cipher):
+#function to count the frequency of the cipher 
+def cryptAnalysis(plainText, cipher):
     pool = list(string.ascii_lowercase + string.digits + string.punctuation + string.whitespace)
-    counted = Counter(cipher)
-    real_count_dict = {k: counted[k] for k in pool if k in counted}
-    return real_count_dict
+
+    cipherTextFrequencyCounter = Counter(cipher)
+    plainTextFrequencyCounter = Counter(plainText)
+
+    cipher_count_dict = {k: cipherTextFrequencyCounter[k] for k in pool if k in cipherTextFrequencyCounter}
+    plain_count_dict = {l: plainTextFrequencyCounter[l] for l in pool if l in plainTextFrequencyCounter}
+
 
 #main func 
 def main():
@@ -51,18 +52,32 @@ def main():
         quit()
 
     oFile = open(sys.argv[1], 'r') #if passed; the file is opened to read
-    fileContent = oFile.read()
+    fileContent = oFile.read() #plain text content
     encryptedText = encrypt(fileContent, encryption_key_pair) #content of the opend file is encrypted
     oFile.close()
 
-    realTextCount = realTextCounter(fileContent)
-    cryptCount = cryptAnalysis(encryptedText)
+    guessedKey = {"a":'7', "b": '5'}
+    print('cipher is : ')
+    print(encryptedText)
 
-    guessedKey = dict(zip(cryptCount, realTextCount))
-
-    print(decryption_key_pair)
-    print("--------------------------------------------------------------------------------")
-    print(decrypt(encryptedText, guessedKey))
+    while True:
+        print('''
+        1 : show true decryption key
+        2: decrypt cipher using true decryption key
+        3: show guessed key(s)
+        0: exit the program
+        ''')
+        userInput = str(input("enter your choice: "))
+        if userInput == '0':
+            break
+        elif userInput == '1':
+            print(decryption_key_pair)
+        elif userInput == '2':
+            print(decrypt(encryptedText, decryption_key_pair))
+        elif userInput == '3':
+            print(decrypt(encryptedText, guessedKey))
+        else:
+            print('unknown input, try again!')
 
 #if not imported
 if __name__ == '__main__':
