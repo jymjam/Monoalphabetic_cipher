85ce64030d9f4ccb2862050a525b7214039ae69c
white-panda01 <hars0101@gmail.com>
2021-10-09 17:09:57 +0400

    implemented certain substitution key guesser

--- cipher.py
+++ cipher.py
@@ -104,6 +104,7 @@ def main():
         elif userInput == '2':
             print(decrypt(encryptedText, decryption_key_pair))
         elif userInput == '3':
+            print("Program confidently guesses following to be accurate substitution for decryption\n")
             print(cryptAnalysis(fileContent, encryptedText)[2])
         elif userInput == '4':
             print(encryptedText)
